3ab0fbab0105861ecd177eaea93f12c1d7ea9459
nikoncsuti <nikoncsuti@gmail.com>
2021-06-08 21:58:13 +0200

    Update Titkosito.py

---
+++ Titkosito.py
@@ -0,0 +1,11 @@
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+
+with open('mykey.key', 'wb') as mykey:
+    mykey.write(key)
+
+with open('mykey.key', 'rb') as mykey:
+    key = mykey.read()
+
+print(key)
